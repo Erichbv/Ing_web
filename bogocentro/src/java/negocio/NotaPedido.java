ca7eeb9bb85496a998fa455ddb9210bfd8bce912
TeresaLicito <mariteresa97@gmail.com>
2016-11-28 21:07:34 -0500

    capa de negocio
    revisalo amor porfavor

--- bogocentro/src/java/negocio/NotaPedido.java
+++ bogocentro/src/java/negocio/NotaPedido.java
@@ -1,14 +1,75 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package negocio;
 
-/**
- *
- * @author marit
- */
 public class NotaPedido {
-    
+   private DetallePedido detaPed;
+   private double total;
+   private int cantidad;
+   private Usuario Usuario;
+   private String NumPedido, fecha, hora, cancelarPedido;
+
+    public DetallePedido getDetaPed() {
+        return detaPed;
+    }
+
+    public void setDetaPed(DetallePedido detaPed) {
+        this.detaPed = detaPed;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Usuario getUsuario() {
+        return Usuario;
+    }
+
+    public void setUsuario(Usuario Usuario) {
+        this.Usuario = Usuario;
+    }
+
+    public String getNumPedido() {
+        return NumPedido;
+    }
+
+    public void setNumPedido(String NumPedido) {
+        this.NumPedido = NumPedido;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getCancelarPedido() {
+        return cancelarPedido;
+    }
+
+    public void setCancelarPedido(String cancelarPedido) {
+        this.cancelarPedido = cancelarPedido;
+    }
+   
 }
--- bogocentro/src/java/negocio/Usuario.java
+++ bogocentro/src/java/negocio/Usuario.java
@@ -1,14 +1,73 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package negocio;
 
-/**
- *
- * @author marit
- */
+package negocio;
 public class Usuario {
-    
+    private String codUsuario, nomUsuario; 
+    private String apeUsuario, nickUsuario; 
+    private String pasUsuario, dirUsuario;
+    private String dniUsuario, fechaNac;
+
+    public String getCodUsuario() {
+        return codUsuario;
+    }
+
+    public void setCodUsuario(String codUsuario) {
+        this.codUsuario = codUsuario;
+    }
+
+    public String getNomUsuario() {
+        return nomUsuario;
+    }
+
+    public void setNomUsuario(String nomUsuario) {
+        this.nomUsuario = nomUsuario;
+    }
+
+    public String getApeUsuario() {
+        return apeUsuario;
+    }
+
+    public void setApeUsuario(String apeUsuario) {
+        this.apeUsuario = apeUsuario;
+    }
+
+    public String getNickUsuario() {
+        return nickUsuario;
+    }
+
+    public void setNickUsuario(String nickUsuario) {
+        this.nickUsuario = nickUsuario;
+    }
+
+    public String getPasUsuario() {
+        return pasUsuario;
+    }
+
+    public void setPasUsuario(String pasUsuario) {
+        this.pasUsuario = pasUsuario;
+    }
+
+    public String getDirUsuario() {
+        return dirUsuario;
+    }
+
+    public void setDirUsuario(String dirUsuario) {
+        this.dirUsuario = dirUsuario;
+    }
+
+    public String getDniUsuario() {
+        return dniUsuario;
+    }
+
+    public void setDniUsuario(String dniUsuario) {
+        this.dniUsuario = dniUsuario;
+    }
+
+    public String getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(String fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
 }
